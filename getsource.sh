133b9b2b05dcebb9e3730bd11e544c922e4b2435
Joseph Turian <turian@gmail.com>
2010-04-22 09:45:32 -0500

    Simplified getsource.sh

--- getsource.sh
+++ getsource.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-cd ~/webapps/$OSQA_APPNAME
-mkdir projects
-cd projects
-svn co http://svn.osqa.net/svnroot/osqa/trunk osqa
-cd osqa
+mkdir -p $OSQA_PROJECTDIR
+svn co http://svn.osqa.net/svnroot/osqa/trunk $OSQA_PROJECTDIR
+cd $OSQA_PROJECTDIR
 
 # We need python2.5 to be compatible with WSGI
-python2.5 ~/utils/bin/pip install -E ~/webapps/$OSQA_APPNAME/envs/ -r osqa-requirements.txt
-source ~/envs/osqa/bin/activate
+python2.5 ~/utils/bin/pip install -E $OSQA_ENVDIR -r osqa-requirements.txt
+source $OSQA_ENVDIR
 
 # [Optional] If you want a MySQL database
-easy_install-2.5 --prefix ~/webapps/$OSQA_APPNAME/envs/ mysql-python
+easy_install-2.5 --prefix $OSQA_ENVDIR mysql-python
--- makeenv.py
+++ makeenv.py
@@ -4,6 +4,8 @@ import os
 import globals
 
 for var in dir(globals):
+    if var == "os" or var == "os.environ": continue
+
     if var[0] == "_": continue
     val = globals.__dict__[var]
     if val is None: val = "None"
